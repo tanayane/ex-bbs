01c0a9da01711aa93bff61b642d3e3642da2fc29
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-06-04 17:07:35 +0900

    記事一覧検索をラムダ式に変換

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -1,12 +1,10 @@
 package com.example.repository;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 //import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -28,10 +26,10 @@ import com.example.domain.Comment;
 @Repository
 @Transactional
 public class ArticleRepository {
-
+	
 	@Autowired
 	NamedParameterJdbcTemplate template;
-
+	
 //	/**
 //	 * DBからの記事情報を格納する.
 //	 */
@@ -42,68 +40,68 @@ public class ArticleRepository {
 //		article.setContent(rs.getString("content"));
 //		return article;
 //	};
-
+	
 	/**
 	 * 記事を全件検索する.
 	 * 
 	 * @return 記事のリスト
 	 */
-	public List<Article> findAll() {
-		String sql = "select a.id ,a.name,a.content,c.id as com_id,c.name as com_name,c.content as com_content "
+	public List<Article> findAll(){
+		String sql="select a.id ,a.name,a.content,c.id as com_id,c.name as com_name,c.content as com_content "
 				+ "from articles a left outer join comments c on(a.id=c.article_id) order by a.id desc ,c.id desc";
-		List<Article> articleList = template.query(sql, new ResultSetExtractor<List<Article>>() {
-
-			public List<Article> extractData(ResultSet rs) throws SQLException, DataAccessException {
-				List<Article> artList = new ArrayList<>();
-				Article article = null;
+			
+			ResultSetExtractor<List<Article>> resultSetExtractor=(ResultSet rs)->{
+				List<Article> artList =new ArrayList<>();
+				Article article=null;
 				List<Comment> commentList = null;
-				int flagId = 0;
-
-				while (rs.next()) {
-					if (flagId != rs.getInt("id")) { // 別の記事になったとき
-						article = new Article();
+				int flagId=0;
+			
+				while(rs.next()) {
+					if(flagId != rs.getInt("id")) {	    //別の記事になったとき
+						article=new Article();
 						article.setId(rs.getInt("id"));
 						article.setName(rs.getString("name"));
 						article.setContent(rs.getString("content"));
-						commentList = new ArrayList<>();
+						commentList=new ArrayList<>();
 						article.setCommentList(commentList);
 						artList.add(article);
+						flagId=article.getId();
 					}
-
-					if (rs.getInt("com_id") != 0) { // コメントが1件もない時
-						Comment comment = new Comment();
+					
+					if(rs.getInt("com_id")!=0) {		//コメントが1件もない時
+						Comment comment=new Comment();		  		    
 						comment.setId(rs.getInt("com_id"));
 						comment.setName(rs.getString("com_name"));
 						comment.setContent(rs.getString("com_content"));
 						comment.setArticleId(rs.getInt("id"));
 						commentList.add(comment);
 					}
-
 				}
 				return artList;
-			}
-		});
+			};		
+		List<Article> articleList=template.query(sql, resultSetExtractor);
 		return articleList;
 	}
-
+	
+	
 	/**
 	 * 記事を投稿する.
 	 * 
 	 * @param article 投稿したい記事
 	 */
 	public void insert(Article article) {
-		String sql = "insert into articles (name,content) values (:name,:content)";
-		SqlParameterSource param = new BeanPropertySqlParameterSource(article);
-		template.update(sql, param);
+		String sql="insert into articles (name,content) values (:name,:content)";
+		SqlParameterSource param=new BeanPropertySqlParameterSource(article);
+		template.update(sql, param);	
 	}
-
+	
 	/**
 	 * 記事とその記事についたコメントを削除する.
 	 * 
 	 * @param articleId 削除する記事のid
 	 */
 	public void deleteById(Integer articleId) {
-		String sql = "WITH deleted AS (DELETE FROM articles WHERE id = :articleId RETURNING id) "
+		String sql="WITH deleted AS (DELETE FROM articles WHERE id = :articleId RETURNING id) "
 				+ "DELETE FROM comments WHERE article_id IN (SELECT id FROM deleted)";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("articleId", articleId);
 		template.update(sql, param);
